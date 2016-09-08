7cc2ee9632daa09f52c8383b4da5034651d9823e
Renee <rchar20@gmail.com>
2016-09-07 19:41:10 -1000

    invoked the loan function and assigned return value to stevesLoan

--- foreclosure.js
+++ foreclosure.js
@@ -5,4 +5,66 @@ var stevesLoan;
 var month = 0;
 var monthsUntilEvicted;
 
- 
+var loan = function(){
+	var account = {
+		borrowed: 550000,
+		balance: 286000,
+		monthlyPayment: 1700,
+		defaulted: 0,
+		defaultsToForeclose: 5,
+		foreclosed: false
+	};
+	var missPayment = function() {
+		if (account.defaulted >= account.defaultsToForeclose) {
+			account.foreclosed = true;
+		} else {
+			return account.defaulted+=1;
+		}
+	};
+	return {
+		getBalance: function(){
+			return account.balance;
+		},
+		receivePayment: function(amount) {
+			if(amount < account.monthlyPayment) {
+				missPayment();
+			} else {
+				account.balance -= amount;
+			}
+		},
+		getMonthlyPayment: function(){
+			return account.monthlyPayment;
+		},
+		isForeclosed: function() {
+			return account.foreclosed;
+		}
+
+	};
+};
+
+var borrower = function (loan) {
+	var account = {
+		monthlyIncome: 1350,
+		funds: 2800,
+		loan: loan
+	};
+	return {
+		getFunds: function() {
+			return account.funds;
+		},
+		makePayment: function() {
+			if (account.funds > loan.getMonthlyPayment) {
+				account.funds -= loan.getMonthlyPayment;
+				loan.receivePayment(loan.getMonthlyPayment);
+			} else {
+				loan.receivePayment(account.funds);
+				account.funds = 0;
+			}
+		},
+		payDay: function(){
+			account.funds += account.monthlyIncome;
+		}
+	};
+};
+
+stevesLoan = loan();
